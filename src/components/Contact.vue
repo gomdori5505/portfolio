092d6232ea5d85654c8a4841a82da94ffae038bd
lsk <lsk@lskui-MacBookPro.local>
2021-01-04 12:51:54 +0900

    feat: create contact form and check validation system

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,74 +1,166 @@
 <template>
-    <v-sheet
-        id="contact"
-        color="#333333"
-        dark
-        tag="section"
-        tile
+<v-sheet
+    id="contact"
+    color="#333333"
+    dark
+    tag="section"
+    tile
+  >
+    <div class="py-12"></div>
+
+    <v-container>
+      <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Contact Me</h2>
+
+      <v-responsive
+        class="mx-auto mb-12"
+        width="56"
       >
-        <div class="py-12"></div>
+        <v-divider class="mb-1"></v-divider>
 
-        <v-container>
-          <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Contact Me</h2>
-
-          <v-responsive
-            class="mx-auto mb-12"
-            width="56"
-          >
-            <v-divider class="mb-1"></v-divider>
-
-            <v-divider></v-divider>
-          </v-responsive>
-
+        <v-divider></v-divider>
+      </v-responsive>
+      <v-row>
+        <v-col>aa</v-col>
+        <v-col>
           <v-theme-provider light>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  flat
-                  label="Name*"
-                  solo
-                ></v-text-field>
-              </v-col>
+            <v-form @submit.prevent="submit">
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    :error-messages="nameErrors"
+                    :counter="10"
+                    flat
+                    label="Name*"
+                    name="name"
+                    v-model="name"
+                    required
+                    solo
+                    @input="$v.name.$touch()"
+                    @blur="$v.name.$touch()"
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="12">
-                <v-text-field
-                  flat
-                  label="Email*"
-                  solo
-                ></v-text-field>
-              </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    :error-messages="emailErrors"
+                    flat
+                    label="Email*"
+                    name="email"
+                    v-model="email"
+                    required
+                    solo
+                    @input="$v.email.$touch()"
+                    @blur="$v.email.$touch()"
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="12">
-                <v-text-field
-                  flat
-                  label="Subject*"
-                  solo
-                ></v-text-field>
-              </v-col>
+                <v-col cols="12">
+                  <v-textarea
+                    :error-messages="messageErrors"
+                    flat
+                    label="Message*"
+                    name="message"
+                    v-model="message"
+                    required
+                    solo
+                    @input="$v.name.$touch()"
+                    @blur="$v.name.$touch()"
+                  ></v-textarea>
+                </v-col>
 
-              <v-col cols="12">
-                <v-textarea
-                  flat
-                  label="Message*"
-                  solo
-                ></v-textarea>
-              </v-col>
-
-              <v-col
-                class="mx-auto"
-                cols="auto"
-              >
-                <v-btn
-                  color="accent"
-                  x-large
+                <v-col
+                  class="mx-auto"
+                  cols="auto"
                 >
-                  Submit
-                </v-btn>
-              </v-col>
-            </v-row>
+                  <v-btn
+                    color="accent"
+                    x-large
+                    type="submit"
+                  >
+                    보내기
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
           </v-theme-provider>
-        </v-container>
-
-        <div class="py-12"></div>
-      </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="py-12"></div>
+  </v-sheet>
 </template>
+
+<script>
+import emailjs from 'emailjs-com';
+import { required, maxLength, email } from 'vuelidate/lib/validators'
+
+export default {
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    email: { required, email },
+    message: { required }
+  },
+
+  data: () => ({
+    name: '',
+    email: '',
+    message: '',
+    serviceId: 'service_3mlzt69',
+    templateId: 'template_ch82n08',
+    userId: 'user_HikCziUGITuc0gljgBzeW'
+  }),
+
+  computed: {
+    nameErrors () {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      !this.$v.name.maxLength && errors.push('이름은 10자 이하여야 합니다.')
+      !this.$v.name.required && errors.push('이름은 필수입니다.')
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('이메일이 유효하지 않습니다.')
+      !this.$v.email.required && errors.push('이메일은 필수입니다.')
+      return errors
+    },
+    messageErrors () {
+      const errors = []
+      if (!this.$v.message.$dirty) return errors
+      !this.$v.message.required && errors.push('메세지는 필수입니다.')
+      return errors
+    }
+  },
+
+  methods: {
+    submit (e) {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+        console.log("error")
+      } else {
+        // do your submit logic here
+        this.submitStatus = 'PENDING'
+        console.log("pending")
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+          console.log("ok")
+          this.sendEmail(e, this.serviceId, this.templateId, this.userId);
+        }, 500)
+      }
+    },
+
+    sendEmail: (e, serviceId, templateId, userId) => {
+      emailjs.sendForm(serviceId, templateId, e.target, userId)
+        .then((result) => {
+          console.log('SUCCESS!', result.status, result.text);
+          alert("메세지를 성공적으로 전송하였습니다.")
+        }, (error) => {
+          console.log('FAILED...', error);
+          alert("메세지 전송에 실패하였습니다.")
+        });
+    }
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import vueTypingAnimation from "./typing.animation.module"
 import { library } from '@fortawesome/fontawesome-svg-core'
-
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import {} from '@fortawesome/free-regular-svg-icons'
 import { faHtml5, faCss3, faBootstrap, faSass, faJs, faVuejs, faPhp } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vuelidate from 'vuelidate'
 
 library.add( faHtml5, faCss3, faBootstrap, faSass, faJs, faVuejs, faPhp, faDatabase )
 
@@ -17,6 +17,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 Vue.use(vueTypingAnimation)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
